75c59ec2affd21f2ccb9ef07976f43f33b65fb75
renan.silva <renan.silva@info-03.nhj.net.rj>
2017-03-22 15:00:02 -0300

    Logout funcionando

--- smartcompras/src/main/java/br/org/smartcompras/config/WebSecurityConfig.java
+++ smartcompras/src/main/java/br/org/smartcompras/config/WebSecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import br.org.smartcompras.service.CostumerUserDetails;
 
@@ -42,8 +43,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         .loginPage("/login").defaultSuccessUrl("/sistema", true)
         .permitAll()
         .and()
-    .logout()
-        .permitAll();
+//    .logout()
+//        .permitAll();
+        .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login");
 		
 	}
 
